fbd10f5a04788817058875e4a3f8d31582590dde
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2023-03-22 23:04:07 +0100

    test

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ for url in ctadata.list_dir("lst/DL1/20241114/v0.1/headcut"):
         print("stored", ctadata.fetch_and_save_file(url)/1024/1024, "Mb")
         print("found keys", h5py.File(url.split("/")[-1]).keys())
 ```
+
+
+## Known Issues
+
+* directory listing shows some not useful information
+  * shows also top level directories and other links, this can be misleading
+  * shows some duplicates
--- ctadata/__init__.py
+++ ctadata/__init__.py
@@ -4,44 +4,74 @@ from urllib.parse import urljoin
 
 import requests
 
+__version__ = ""
+
 logger = logging.getLogger(__name__)
 
-# https://daskhub.dev.ctaodc.ch/services/downloadservice/fetch/pnfs/cta.cscs.ch/lst/DL1/20230226/v0.9_calib12100/tailcut84
-
-hubbase = "http://hub:5000"
-dsbase = urljoin(hubbase, "services/downloadservice/fetch/pnfs/cta.cscs.ch/")
-altdsbase = urljoin(hubbase, "fetch/pnfs/cta.cscs.ch/")
+default_downloadservice = os.getenv("CTADS_URL", "http://hub:5000/services/downloadservice/")
 
 
-def list_dir(url):
-    fullurl = urljoin(dsbase, url)
+fetch_endpoint = "fetch/"
+data_root = "pnfs/cta.cscs.ch/"
+
+optional_url_parts = ["services/downloadservice/"]
+
+
+def get(url, token=None, downloadservice=None, stream=False):
+    if token is None:
+        token = os.getenv("JUPYTERHUB_API_TOKEN")
+
+    if downloadservice is None:
+        downloadservice = default_downloadservice
+
+    fullurl = urljoin(urljoin(urljoin(downloadservice, fetch_endpoint), data_root), url)
+
+    logger.info("full url: %s", fullurl)
+    logger.info("token: %s", token)
     
-    r = requests.get(
-                fullurl,
-                params = {'token': os.getenv("JUPYTERHUB_API_TOKEN")},
-            )
+    params = {'token': token} #, 'ctadata_version': __version__}
+
+    return requests.get(fullurl, params=params, stream=stream)
+
+
+def list_dir(url, token=None, downloadservice=None):
+    r = get(url, token, downloadservice)
+    
     try:
-        return [u.replace(dsbase, "").replace(altdsbase, "") for u in r.json()['urls']]
+        if downloadservice is None:
+            downloadservice = default_downloadservice
+
+        urls = []
+        for u in r.json()['urls']:
+            u = u.replace("http://", "https://")
+            u = u.replace(downloadservice + fetch_endpoint + data_root, "")
+            u = u.replace(downloadservice, "")
+            for optional_url_part in optional_url_parts:
+                p = downloadservice.replace(optional_url_part, "")
+                u = u.replace(p + fetch_endpoint + data_root, "")
+                u = u.replace(p, "")                
+
+            if u not in ['', 'fetch//', 'fetch/pnfs/']:
+                urls.append(u)
+
+        return list(sorted(set(urls)))
+    
     except Exception as e:
         return r.text[:1000]
 
 
 
-def fetch_and_save_file(url, fn=None):
+def fetch_and_save_file(url, fn=None, token=None, downloadservice=None):
     total_wrote = 0
 
     if fn is None:
         fn = url.split("/")[-1]
 
-    with open(fn, "wb") as outf:
-        with requests.get(
-            urljoin(dsbase, url),
-            params = {'token': os.getenv("JUPYTERHUB_API_TOKEN")},
-            stream=True
-        ) as f:          
+    with open(fn, "wb") as out_file:
+        with get(url, token, downloadservice) as f:          
             f.raise_for_status()
             for r in f.iter_content(chunk_size=1024*1024):
-                outf.write(r)                
+                out_file.write(r)                
                 total_wrote += len(r)
 
     return total_wrote
