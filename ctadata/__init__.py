c67870a086db081042469558a21eb616ef541e12
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2023-03-28 19:21:00 +0200

    config

--- ctadata/__init__.py
+++ ctadata/__init__.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from urllib.parse import urljoin
+import click
 
 import requests
 
@@ -27,9 +28,9 @@ def get(url, token=None, downloadservice=None, stream=False):
     fullurl = urljoin(urljoin(urljoin(downloadservice, fetch_endpoint), data_root), url)
 
     logger.info("full url: %s", fullurl)
-    logger.info("token: %s", token)
+    logger.debug("token: %s", token)
     
-    params = {'token': token} #, 'ctadata_version': __version__}
+    params = {'token': token, 'ctadata_version': __version__}
 
     return requests.get(fullurl, params=params, stream=stream)
 
@@ -39,7 +40,7 @@ def list_dir(url, token=None, downloadservice=None):
     
     try:
         if downloadservice is None:
-            downloadservice = default_downloadservice
+            downloadservice = default_downloadservice.replace("http://", "https://")
 
         urls = []
         for u in r.json()['urls']:
@@ -68,12 +69,34 @@ def fetch_and_save_file(url, fn=None, token=None, downloadservice=None):
         fn = url.split("/")[-1]
 
     with open(fn, "wb") as out_file:
-        with get(url, token, downloadservice) as f:          
-            f.raise_for_status()
-            for r in f.iter_content(chunk_size=1024*1024):
-                out_file.write(r)                
-                total_wrote += len(r)
+        # with get(url, token, downloadservice, stream=True) as f:          
+        f = get(url, token, downloadservice, stream=True)
+        logger.info("got response %s", f)
+        f.raise_for_status()
+        for r in f.iter_content(chunk_size=1024*1024):
+            logger.info("total wrote %s Mb", total_wrote/1024/1024)
+            out_file.write(r)                
+            total_wrote += len(r)
 
     return total_wrote
         
-    
+
+@click.group
+def main():
+    logging.basicConfig(level='INFO')
+
+@main.command("list")
+@click.argument("path", type=str)
+def list_path(path):
+    r = list_dir(path)
+
+    if isinstance(r, list):
+        for fn in r:
+            click.echo(fn)
+    else:
+        logger.warning("problem listing files: %s", r)
+
+@main.command("get")
+@click.argument("path", type=str)
+def get_path(path):
+    fetch_and_save_file(path)
